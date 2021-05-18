f046e915c8b836ab67836b094834a96e5ef88c18
shaunjacob <46585856+shaunjacob@users.noreply.github.com>
2021-05-18 14:37:45 +1000

    images

---
+++ Start-ComplianceScan.ps1
@@ -0,0 +1,34 @@
+
+$connectionName = "AzureRunAsConnection"
+
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Connect-AzAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint -Subscription $servicePrincipalConnection.SubscriptionId 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
+
+$Start = Get-Date
+
+Start-AzPolicyComplianceScan
+
+$Stop = Get-Date
+$TimeTaken = ($Stop - $Start).TotalSeconds
+Write-Output "The time to run this script was $TimeTaken seconds"
+Write-Output "Done"
